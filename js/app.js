965b929aacaf6585904d3695b95e105c33c5cb38
Harpmen <harpvad@gmail.com>
2021-07-28 22:02:37 +0300

    working version 1.0

---
+++ js/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+function calculateTotalProfit(depositAmount, period) {
+    let percentEl = document.getElementById('percent')
+    if (period < 6) {
+        percentEl = 2;
+    }
+    else if (period < 9) {
+        percentEl = 2.2;
+    }
+    else if (period < 12) {
+        percentEl = 2.3
+    }
+    else if (period <= 18) {
+        percentEl = 2.6
+    }
+    const profit = depositAmount * percentEl / 100 * (period / 12);
+    const totalProfit = depositAmount + profit;
+
+    return {
+        profit,
+        totalProfit,
+        percentEl,
+    };
+}
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+
+    totalEl.textContent = '';
+    profitEl.textContent = '';
+    percentEl.textContent = '';
+
+    const depositAmount = Number(depositAmountInputEl.value);
+    const period = Number(periodInputEl.value);
+
+    const result = calculateTotalProfit(depositAmount, period);
+    totalEl.textContent = result.totalProfit.toFixed(0);
+    profitEl.textContent = result.profit.toFixed(0);
+    percentEl.textContent = result.percentEl.toFixed(1);
+
+}
+
+const formEl = document.getElementById('deposit-form');
+formEl.onsubmit = handleSubmit;
+
+const depositAmountInputEl = document.getElementById( 'amount-input');
+const periodInputEl = document.getElementById('period-input');
+const totalEl = document.getElementById('total');
+const profitEl = document.getElementById('profit');
+const percentEl = document.getElementById('percent');
